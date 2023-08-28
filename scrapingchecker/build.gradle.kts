e9dc59824815dcd5b86acd4059d17e0e4d2032b5
Frosch2010 <jens.muen@web.de>
2023-08-28 22:54:13 +0200

    ADD: Publishing to jitpack.io

--- scrapingchecker/build.gradle.kts
+++ scrapingchecker/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -34,10 +35,39 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["release"])
+
+            groupId = "com.frosch2010"
+            artifactId = "scrapingchecker"
+            version = "1.0.0"
+
+            pom {
+                name.set("ScrapingChecker")
+                description.set("ScrapingChecker is an Android library that simplifies web scraping permission validation by parsing robots.txt content, making it easy to determine if scraping is allowed for a given URL.")
+                url.set("https://github.com/frosch2010/ScrapingChecker")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
 }
