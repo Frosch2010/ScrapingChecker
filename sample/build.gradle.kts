1b090a1dfc64afbe2866d1167728d39b130f3cc9
Frosch2010 <jens.muen@web.de>
2023-09-05 23:33:48 +0200

    CHG: version

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.frosch2010.scrapingchecker.sample"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
